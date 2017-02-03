6802c04a30c7f68d76bddf79ac39163cf3b5cff4
Nader GHADDAR <nghaddar@e2r11p4.42.fr>
2017-02-03 15:35:54 +0100

    minor fixes

--- get_next_line.c
+++ get_next_line.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#include "libft.h"
 
 void	clean_buffer(char **line, char *buffer)
 {
